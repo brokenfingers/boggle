87dd086038423e81b9c60ebfa9de683b68104f9a
Hyun Sik Kang <hyunsikk91@gmail.com>
2016-10-01 23:51:04 -0700

    add some more styling

--- app/client/components/page/home.jsx
+++ app/client/components/page/home.jsx
@@ -12,15 +12,17 @@ export default class Home extends Component {
       <Application>
         <div className='container'>
           <div className='jumbotron home-jumbotron'>
-            <div>
-              <h1>Boggle</h1>
-              <h3>
-                Word game designed by Allan Turoff and originally distributed by Parker Brothers.
-                The game is played using a plastic grid of lettered dice, in which players attempt to find
-                words in sequences of adjacent letters.
-              </h3>
+            <div className='bg-overlay'>
+              <div className='container'>
+                <h1>Boggle</h1>
+                <h3>
+                  Word game designed by Allan Turoff and originally distributed by Parker Brothers.
+                  The game is played using a plastic grid of lettered dice, in which players attempt to find
+                  words in sequences of adjacent letters.
+                </h3>
+                <Link to='/play' className='btn btn-primary home-btn'>Play</Link>
+              </div>
             </div>
-            <Link to='/play' className='btn btn-primary home-btn'>Play</Link>
           </div>
         </div>
       </Application>
